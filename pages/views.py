32dd386edcac55fb0127ea4618cd8ac1b5835902
kent <abitonakent923@gmail.com>
2025-09-17 10:52:28 +0800

    update

---
+++ pages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    context = {
+        'title': 'Home Page',
+        'features': [
+            'Django', 'Templates', 'Static Files'
+        ]
+    }
+    return render(request, 'home.html', context)
+
+def about(request):
+    return render(request, 'about.html', {'title': 'about'})
